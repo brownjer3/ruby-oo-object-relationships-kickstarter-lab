acac96296727f6ca16337c61c85cfcc843b24d00
jerry <brownjer3@gmail.com>
2020-11-04 16:37:19 -0800

    commit tree 079fa8227bd161e15951744443cf601332b8afb6

---
+++ lib/project.rb
@@ -0,0 +1,23 @@
+class Project
+    attr_reader :title
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        ProjectBacker.all.select {|p| p.project == self}.collect {|a| a.backer}
+    end
+
+end
